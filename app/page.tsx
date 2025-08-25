aa188f64b8e84dec2a1bd5eb8cfa16642aabb621
mohitlakhara-ind <mohitlakhara78@gmail.com>
2025-08-25 21:57:01 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
-import { lusitana, Lusitana } from "@/app/ui/fonts";
+import { lusitana } from "@/app/ui/fonts";
 import styles from "@/app/ui/home.module.css";
 export default function Page() {
   return (
